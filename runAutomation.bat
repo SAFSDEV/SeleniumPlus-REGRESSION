0f62b52b3dc2635b09e326dc6f209618c56f450b
leiwang008 <lei.wang@sas.com>
2015-11-19 11:23:35 +0800

    Compile the regression project firstly.

--- runAutomation.bat
+++ runAutomation.bat
@@ -7,10 +7,22 @@ for /f "tokens=1* delims=-.0" %%A in ('dir /b /a-d %SELENIUM_PLUS%\libs\selenium
 set SELENIUM_SERVER_JAR_LOC=%SELENIUM_PLUS%\libs\selenium-%max%
 set CMDCLASSPATH="%SELENIUM_PLUS%\libs\seleniumplus.jar;%SELENIUM_PLUS%\libs\JSTAFEmbedded.jar;%SELENIUM_SERVER_JAR_LOC%"
 set EXECUTE=%SELENIUM_PLUS%\Java\jre\bin\java.exe
+set COMPILE=%SELENIUM_PLUS%\Java\bin\javac.exe
 
 :: DON'T MODIFY ABOVE SETTING UNLESS NECESSARY
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: First compile the project
+rmdir /s /q bin
+mkdir bin
+"%COMPILE%" -cp %CMDCLASSPATH% -encoding UTF-8 -d bin -nowarn Tests/regression/*.java Tests/regression/testcases/*.java Tests/regression/testruns/*.java
+Echo "Project has been compiled." 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Run the regression test 
+ 
 :: How to override App Map variable
 :: EXAMPLE:  %EXECUTE% -cp %CMDCLASSPATH%;bin regression.testruns.Regression -safsvar:GoogleUser=email@gmail.com
 
@@ -20,3 +32,4 @@ set EXECUTE=%SELENIUM_PLUS%\Java\jre\bin\java.exe
 "%EXECUTE%" -cp %CMDCLASSPATH%;bin regression.testruns.Regression
 
 Echo "Test Job is Done."
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
