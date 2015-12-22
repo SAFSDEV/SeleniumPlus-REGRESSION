3bd96802d13c63dddd47e336dddb66d63570a86a
leiwang008 <lei.wang@sas.com>
2015-12-22 15:28:06 +0800

    Add regression test for "AUTOIT".

---
+++ Tests/regression/testcases/AutoItTests.java
@@ -0,0 +1,185 @@
+/** 
+ * Copyright (C) SAS Institute, All rights reserved.
+ * General Public License: http://www.opensource.org/licenses/gpl-license.php
+ */
+
+/**
+ * regression.testcases.AutoItTests.java:
+ * Logs for developers, not published to API DOC.
+ *
+ * History:
+ * 18 DEC 2015    (Lei Wang) Initial release.
+ */
+package regression.testcases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.safs.Domains;
+import org.safs.StringUtils;
+import org.safs.autoit.AutoIt;
+import org.safs.autoit.AutoItRs;
+import org.safs.model.tools.EmbeddedHookDriverRunner;
+
+import regression.Map;
+import regression.testruns.Regression;
+import autoitx4java.AutoItX;
+
+/**
+ * This class is used to test implementation of AUTOIT.<br>
+ * It is supposed to run ONLY on Windows Operating System.<br>
+ *
+ */
+public class AutoItTests extends Regression{
+	public static final String COUNTER = StringUtils.getClassName(0, false);
+	static Utilities utils = null;
+	public static final String MAP_FILE_AUTOITAPP = "AutoItApp.map";
+
+	/**
+	 * Test with Windows calculator application.
+	 * @return int, the number of unexpected failure.
+	 * @throws Throwable
+	 */
+	private static int testCaculator() throws Throwable{
+		int fail = 0;
+		String applicationID = "calculator";
+		String executableCalc = "calc.exe";
+
+		if(Misc.LaunchApplication(applicationID, executableCalc)){
+
+			if(Window.SetFocus(Map.Calculator.Calculator)){
+				//make an addition "1+2"
+				if(!Click(Map.Calculator.Btn1)) trace(++fail); else Pause(1);
+				if(!Click(Map.Calculator.BtnPlus)) trace(++fail); else Pause(1);
+				if(!Click(Map.Calculator.Btn2)) trace(++fail); else Pause(1);
+				if(!Click(Map.Calculator.BtnEqual)) trace(++fail); else Pause(1);
+
+			}else{
+				trace(++fail);
+			}
+
+			if(!Misc.CloseApplication(applicationID)) trace(++fail);
+		}else{
+			trace(++fail);
+		}
+		
+		Pause(1);
+		return fail;
+	}
+	
+	/**
+	 * Test with Windows notepad application.
+	 * @return int, the number of unexpected failure.
+	 * @throws Throwable
+	 */
+	private static int testNotepad() throws Throwable{
+		int fail = 0;
+		String applicationID = "notepad";
+		String executableNotepad = "notepad.exe";
+		AutoItX it = null;
+		AutoItRs panel = null;
+
+		if(Misc.LaunchApplication(applicationID, executableNotepad)){
+			try{
+				it = AutoIt.AutoItObject();
+			}catch(Exception e){
+				Logging.LogTestWarning("Fail to get instance of AUTOIT. Met "+StringUtils.debugmsg(e));
+			}
+			try{
+				String winrs = Misc.GetAppMapValue(Map.Notepad.Notepad);
+				String comprs = Misc.GetAppMapValue(Map.Notepad.EditBox);
+				panel = new AutoItRs(winrs, comprs);
+			}catch(Exception e){
+				Logging.LogTestWarning("Fail to initialize the components. Met "+StringUtils.debugmsg(e));
+			}
+			
+			if(Window.SetFocus(Map.Notepad.Notepad)){
+				//input some string and verify the input characters.
+				String text = "Value to input";
+				if(EditBox.SetTextValue(Map.Notepad.EditBox, text)){
+					try{
+						//Try to use AUTOIT to call its API directly, to verify the 'SetTextValue'
+						String result = it.controlGetText(panel.getWindowsRS(), "", panel.getComponentRS());
+						if(!text.equals(result)){
+							Logging.LogTestWarning("Notepad verification failed.");
+						}
+
+					}catch(Exception e){
+						Logging.LogTestWarning("Fail to verify the editbox value. Met "+StringUtils.debugmsg(e));
+					}
+					Misc.Pause(1);
+										
+				}else{
+					trace(++fail);
+				}
+
+			}else{
+				trace(++fail);
+			}
+
+			Misc.Pause(1);
+			Misc.CloseApplication(applicationID);
+		}else{
+			trace(++fail);
+		}
+
+		return fail;
+	}
+
+	/**
+	 * <B>NOTE:</B>
+	 * <pre>
+	 * 1. The static field 'utils' should be initialized, utils = new Utilities(Runner.jsafs());
+	 * </pre>
+	 * 
+	 * @param Runner EmbeddedHookDriverRunner
+	 * @return int, the number of unexpected failure.
+	 * @throws Throwable
+	 */
+	public static int runRegressionTest(EmbeddedHookDriverRunner Runner, List<String> enabledDomains) throws Throwable{
+		int fail = 0;
+		Counters.StartCounter(COUNTER);
+
+		try{
+			for(String domain: enabledDomains) Domains.enableDomain(domain);
+			utils = new Utilities(Runner.jsafs());
+			
+			String mapID = MAP_FILE_AUTOITAPP;
+			if(Misc.SetApplicationMap(mapID)){
+				
+				fail += testCaculator();
+				fail += testNotepad();
+				
+				if(!Misc.CloseApplicationMap(mapID)) trace(++fail);
+				
+			}else{
+				trace(++fail);
+				Logging.LogTestFailure(COUNTER+"Fail to load map '"+mapID+"', cannot "+StringUtils.debugmsg(false));
+			}
+
+		}catch(Throwable t){
+			trace(++fail);
+			Logging.LogTestFailure(COUNTER +" fatal error due to "+t.getClass().getName()+", "+ t.getMessage());
+		}
+
+		Counters.StopCounter(COUNTER);
+		Counters.StoreCounterInfo(COUNTER, COUNTER);
+		Counters.LogCounterInfo(COUNTER);
+
+		if(fail > 0){
+			Logging.LogTestFailure(COUNTER+" reports "+ fail +" UNEXPECTED test failures!");
+		}else{
+			Logging.LogTestSuccess(COUNTER+" did not report any UNEXPECTED test failures!");
+		}
+		return fail;
+	}
+
+	public void runTest() throws Throwable{
+		List<String> enabledDomains = new ArrayList<String>();
+		enabledDomains.add(Domains.HTML_DOMAIN);
+		enabledDomains.add(Domains.HTML_DOJO_DOMAIN);
+		enabledDomains.add(Domains.HTML_SAP_DOMAIN);
+
+		runRegressionTest(Runner, enabledDomains);
+	}
+}
